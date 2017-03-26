79b24e2f2963a16581434d993d3ae02b45a87b57
Oleg Klyuchkin <oleg.kluchkin+github@gmail.com>
2017-03-26 19:31:27 +0300

    Add debug output Trivial optimization

--- rss.py
+++ rss.py
@@ -2,11 +2,18 @@ import requests
 import os
 import re
 import json
+import argparse
 from yaml import load as yaml_load
 from xml.etree import ElementTree
 
 with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.yml'), 'r') as yaml_config:
     config = yaml_load(yaml_config)
+
+# Preparing arguments
+argparser=argparse.ArgumentParser(description='Rescan Lostfilm.TV RSS feed for updates')
+argparser.add_argument('-d','--debug', help='debug output', action="store_true")
+args = argparser.parse_args()
+
 
 cookies=';'.join([cookie + '=' + str(config['auth'][cookie]) for cookie in config['auth']])
 transmission_url = 'http://' + str(config['transmission']['host']) + ':' + str(config['transmission']['port']) \
@@ -28,11 +35,13 @@ def transmission_rpc_request(data):
                 auth=(config['transmission']['user'], config['transmission']['password'])
             )
         if torrent_request.status_code == 401:
-            print('Unauthorized')
+            if args.debug:
+                print('Unauthorized')
             exit(1)
    if torrent_request.status_code == 401:
-       print('Unauthorized')
-       exit(1)
+        if args.debug:
+            print('Unauthorized')
+        exit(1)
    return (json.loads(torrent_request.text))
 request_available_torrents=transmission_rpc_request(
     json.dumps({
@@ -52,12 +61,14 @@ if request_available_torrents.get('result')=='success':
         series = data.split('.')[-2]
         name = data.replace(quality,'').replace(series,'').strip('.').replace('.',' ')
         if name not in catalog:
-            catalog.update({name: [series] })
+            catalog.update({name: {series} })
         else:
-            catalog[name].append(series)
+            catalog[name].add(series)
 else:
-    print('Can not send request to transmission')
+    if args.debug:
+        print('Can not send request to transmission')
     exit(1)
+
 list_request = requests.get(config['url'])
 list_request.encoding = 'utf-8'
 rss_items = ElementTree.fromstring(list_request.text).find('channel').findall('item')
@@ -69,22 +80,34 @@ for item in rss_items:
     if search_real_name:
         real_name = search_real_name.group(0).strip('()')
         if real_name not in config['subscriptions']:
+            if args.debug:
+                print("Skipped (not subscribed): " + title)
             continue
     else:
+        if args.debug:
+            print("Skipped (can't find name): " + title)
         continue
     search_quality = re.search("\[.*\]", title.split('.')[1])
     if search_quality:
         quality = search_quality.group(0).strip('[]')
         if quality != config['subscriptions'][real_name]:
-             continue
+            if args.debug:
+                print("Skipped (wrong quality): " + title)
+            continue
     else:
+        if args.debug:
+            print("Skipped (can't detect quality): " + title)
         continue
     search_series = re.search("\(.*\)" , title.split('.')[1])
     if search_series:
         series = search_series.group(0).strip('()')
     else:
+        if args.debug:
+            print("Skipped (can't parse series number): " + title)
         continue
     if real_name in catalog and series in catalog[real_name]:
+        if args.debug:
+            print("Skipped (already added): " + title)
         continue
     torrent_rpc = json.dumps({
                             'arguments': {
@@ -93,4 +116,6 @@ for item in rss_items:
                                          },
                             'method': 'torrent-add'
                             })
-    transmission_rpc_request(torrent_rpc)
+    answer = transmission_rpc_request(torrent_rpc)
+    if args.debug:
+        print("Request: %s\nAnswer: %s" % (torrent_rpc,answer))
