ba875d7b8e98300ce41943a52e1a4af0c9dedfd1
Oleg Kluchkin <oleg.kluchkin@gmail.com>
2018-10-13 22:24:51 +0300

    Упорядочена логика и синтаксис. Возможность скачивать все сезоны без предварительной подписки.

--- rss.py
+++ rss.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import requests
-import os
+from pathlib import Path
 import re
 import json
 import logging
@@ -9,10 +9,7 @@ from yaml import load as yaml_load
 from xml.etree import ElementTree
 
 # Парсинг настроек
-with open(
-    os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        'config.yml'), 'r') as yaml_config:
+with open(Path(__file__).resolve().parent / 'config.yml', 'r') as yaml_config:
             config = yaml_load(yaml_config)
 
 # настройка логирования
@@ -21,7 +18,7 @@ logging.basicConfig(
     level=config['verbose'].upper(),
     handlers=[
         RotatingFileHandler(
-            os.path.join(os.path.dirname(os.path.realpath(__file__)), config['log']['path']),
+            Path(__file__).resolve().parent / config['log']['path'],
             maxBytes=config['log']['maxBytes'],
             backupCount=config['log']['backupCount']
         ),
@@ -30,7 +27,7 @@ logging.basicConfig(
 
 
 # Cookie для авторизации на трекере
-cookies = ';'.join([cookie + '=' + str(config['auth'][cookie]) for cookie in config['auth']])
+cookies = ';'.join(['{}={}'.format(cookie, config['auth'][cookie]) for cookie in config['auth']])
 
 # Строка подключения к transmission RPC
 transmission_url = 'http://{host}:{port}/transmission/rpc/'.format(
@@ -44,7 +41,7 @@ transmission_session_id = None
 
 def transmission_rpc_request(rpc_data) -> json:
     global transmission_session_id
-    for counter in range(0, 2):
+    for _ in range(0, 2):
         torrent_request = requests.post(
             transmission_url,
             data=rpc_data,
@@ -56,14 +53,14 @@ def transmission_rpc_request(rpc_data) -> json:
             break
         elif torrent_request.status_code == 401:
             logging.error('Не авторизован в Transmission')
-            exit(1)
+            exit(401)
         torrent_session_search = re.search('X-Transmission-Session-Id: .+?(?=<)',
                                            torrent_request.text)
         if torrent_session_search:
             transmission_session_id = torrent_session_search.group(0).split(':')[1].strip()
     if torrent_request.status_code != 200:
-        logging.error('Не Удаётся выполнть запрос к Transmission')
-        exit(1)
+        logging.error('Не удалось отправить запрос к transmission')
+        exit(torrent_request.status_code)
     return json.loads(torrent_request.text)
 
 
@@ -74,13 +71,12 @@ request_download_root = transmission_rpc_request(
             'fields': ['download-dir']
         },
         'method': 'session-get'
-    })
-)
+    }))
 if request_download_root['result'] == 'success':
-    download_root = request_download_root['arguments']['download-dir']
-    logging.debug("Директория: " + str(download_root))
+    download_root = Path(request_download_root['arguments']['download-dir'])
+    logging.debug("Директория: {}".format(download_root))
 else:
-    logging.error('Не могу отправить запрос к transmission')
+    logging.error('Не удалось отправить запрос к transmission')
     exit(1)
 
 # Формируем каталог уже загруженных файлов
@@ -109,9 +105,9 @@ if request_available_torrents['result'] == 'success':
         else:
             catalog[name].add(series)
 else:
-    logging.error('Не могу отправить запрос к transmission')
+    logging.error('Не удалось отправить запрос к transmission')
     exit(1)
-logging.debug("Каталог: " + str(catalog))
+logging.debug("Каталог: {}".format(catalog))
 
 # Запрос RSS ленты
 list_request = requests.get(
@@ -124,64 +120,65 @@ for item in rss_items:
     title = item.find('title').text
     link = item.find('link').text
 
+    # Парсинг атрибутов раздачи
     search_real_name = re.search(r"(?!S[0-9]+E[0-9]+)(\([a-zA-Z0-9. ']+\))", title)
     if search_real_name:
         real_name = search_real_name.group(0).strip('()')
-        if (
-            real_name not in config['subscriptions']
-            and real_name not in config['subscriptions_season']
-        ):
-            logging.debug("Не подписан <{real_name}>: {title}".format(
-                          real_name=real_name,
-                          title=title))
+        search_quality = re.search(r"\[.+\]", title)
+        if search_quality:
+            quality = search_quality.group(0).strip('[]')
+            search_series = re.search(r"\(S[0-9]+E[0-9]+\)", title)
+            if search_series:
+                series = search_series.group(0).strip('()')
+            else:
+                logging.warning("Не смог найти серию: " + title)
+                continue
+        else:
+            logging.warning("Не смог определить качество: " + title)
             continue
     else:
         logging.warning("Не получилось найти имя: " + title)
         continue
 
-    search_quality = re.search(r"\[.+\]", title)
-    if search_quality:
-        quality = search_quality.group(0).strip('[]')
-        if (real_name in config['subscriptions']
-            and quality != config['subscriptions'][real_name]
-            or (real_name in config['subscriptions_season']
-                and quality != config['subscriptions_season'][real_name])):
-            logging.debug("Не то качество <%s>: %s" % (quality, title, ))
-            continue
+    if (
+        (
+            # Подписка на сезон
+            (
+                series.endswith('E99') and
+                (
+                    quality == config['download_all_seasons'] or
+                    (
+                        real_name in config['subscriptions_season'] and
+                        quality == config['subscriptions_season'][real_name]
+                    )
+                )
+            ) or
+
+            # Подписка на каждую серию отдельно
+            (
+                real_name in config['subscriptions'] and
+                quality == config['subscriptions'][real_name]
+            )
+        ) and not
+        # Ещё не добавляли
+        (
+            real_name in catalog and
+            series in catalog[real_name]
+        )
+    ):
+        logging.info("Добавляем " + title)
+        torrent_rpc = json.dumps({
+            'arguments': {
+                'cookies': cookies,
+                'filename': link,
+                # Имя директории не может оканчиваться точкой
+                'download-dir': download_root / real_name.strip('.')
+            },
+            'method': 'torrent-add'
+        })
+        transmission_rpc_request(torrent_rpc)
     else:
-        logging.warning("Не смог определить качество: " + title)
-        continue
-
-    search_series = re.search(r"\(S[0-9]+E[0-9]+\)", title)
-    if search_series:
-        series = search_series.group(0).strip('()')
-    else:
-        logging.warning("Не смог найти серию: " + title)
-        continue
-    if series.endswith('E99') and real_name not in config['subscriptions_season']:
-        logging.debug("Сезон: " + title)
-        continue
-    logging.debug('Имя: {real_name} Серия: {quality} Качество: {quality}'.format(
-                  real_name=real_name,
-                  series=series,
-                  quality=quality))
-    if real_name in catalog and series in catalog[real_name]:
-        logging.debug("Уже добавлено: " + title)
-        continue
-
-    if real_name in config['subscriptions_season'] and not series.endswith('E99'):
-        logging.debug("Подписаны на сезон: " + title)
-        continue
-
-    # Если удовлетворяет всем условиям, то добавляем в очередь загрузки
-    torrent_rpc = json.dumps({
-        'arguments': {
-            'cookies': cookies,
-            'filename': link,
-            # Имя директории не может оканьчиваться точкой
-            'download-dir': os.path.join(download_root, real_name.strip('.'))
-        },
-        'method': 'torrent-add'
-    })
-    transmission_rpc_request(torrent_rpc)
-    logging.info("Добавлено " + title)
+        logging.debug('Пропуск real_name={real_name}, series={series}, quality={quality}'.format(
+            real_name=real_name,
+            series=series,
+            quality=quality))
